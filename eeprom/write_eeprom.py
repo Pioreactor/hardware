4d557713cfb6afca7216795da479a5bb3c2857e3
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-08-22 13:53:40 -0400

    small updates

--- eeprom/write_eeprom.py
+++ eeprom/write_eeprom.py
@@ -1,18 +1,22 @@
-# util to write to EEPROM our metadata.
-#
-# Requires utils from the following project:
-# > git clone https://github.com/raspberrypi/hats.git
-#
-# 1. Run `cd eepromutils && make && sudo make install` to install eeprom utils if not already done.
-#
-# 2. this Python file should be placed in the raspberrypi/hats/eepromutils directory,
-# along with the eeprom_settings.txt.template file
-# run with sudo, ex:
-#
-# > sudo python3 write_eeprom.py 0.2
-#
-# TODO: it may fail the first time?? Try again.
-#
+"""
+util to write to EEPROM our metadata.
+
+Requires utils from the following project https://github.com/raspberrypi/hats.git
+
+0. `git clone https://github.com/raspberrypi/hats.git && cd hats`
+
+1. Run `cd eepromutils && make && sudo make install` to install eeprom utils if not already done.
+
+2. copy this Python file into the hats/eepromutils directory
+
+3. copy eeprom_settings.txt.template file into the hats/eepromutils directory,
+
+4. `sudo python3 write_eeprom.py 0.2`.
+
+5. After a `sudo reboot`, try  `pio version -v` to confirm that HATs version.
+
+TODO: it may fail the first time?? Try again.
+"""
 import re
 from datetime import datetime
 import click
@@ -29,11 +33,12 @@ def main(version):
     major, minor = version.split(".")
     version_as_hex_string = "0x" + major.zfill(3) + minor
     current_time = datetime.utcnow().isoformat()
-    serial_number = uuid.uuid4() # log me somewhere else
+    serial_number = uuid.uuid4() # assign me externally / log me somewhere else
+    product_name = "Pioreactor HAT"
 
     with open("./pioreactor_eeprom_settings.txt.template", "r") as in_file:
         with open("./pioreactor_eeprom_settings.txt", "w") as out_file:
-            eeprom_text = in_file.read().format(product_version=version_as_hex_string, current_time=current_time, serial_number=serial_number)
+            eeprom_text = in_file.read().format(product_version=version_as_hex_string, current_time=current_time, serial_number=serial_number, product_name=product_name)
             out_file.write(eeprom_text)
     
     # 2. compile to .eep file.
