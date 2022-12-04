58c3d062902bce8dd3037f1de35ce7333bc6ae2b
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-12-04 18:21:59 -0500

    top level readme

--- setup_HAT.sh
+++ setup_HAT.sh
@@ -5,12 +5,14 @@ set -e
 
 export LC_ALL=C
 
-VERSION=1.0
+: ${1?"Usage: $0 HARDWARE_VERSION"}
+
+
 UUID=$(python -c "import uuid;print(str(uuid.uuid4()))")
 echo $UUID
 
 
-python3 hats/eeprom_utils/write_eeprom.py $VERSION $UUID)
+python3 hats/eeprom_utils/write_eeprom.py $HARDWARE_VERSION $UUID)
 
 python3 tests/HAT_tests.py
 
